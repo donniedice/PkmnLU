0167f695223fd188d7f061f48bf2b78de2299ed1
DonnieDice <DonnieDice@ProtonMail.com>
2022-03-15 02:27:36 -0500

    Shadowlands: Patch 9.2
    Updated for Shadowlands: Patch 9.2

--- main.lua
+++ main.lua
@@ -1,10 +1,5 @@
 local a = CreateFrame("Frame")
 a:RegisterEvent("PLAYER_LEVEL_UP")
-a:SetFrameStrata("BACKGROUND");
-a:SetClampedToScreen(true)
-a:SetAllPoints(UIParent)
-a:Hide();
-
 a:SetScript("OnEvent",
 	function(self, event, ...)
 		PlaySoundFile("Interface\\Addons\\PkmnLU\\PkmnLU.ogg", "Master")
